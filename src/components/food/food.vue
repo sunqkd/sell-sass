6f1fe0cfea31eb8d1b222607c375894d6b368266
sunqkd <sunzhaoqkd@163.com>
2018-10-16 22:55:14 +0800

    'update'

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -18,15 +18,29 @@
                         <span class="now">￥{{food.price}}</span>
                         <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                     </div>
-                </div>
-                <div class="cartcontrol-wrapper">
-                    <cartcontrol :food="food"></cartcontrol>
-                </div>
-                <transition name="fade">
-                    <div class="buy"  v-show="!food.count  || food.count === 0" @click="addFirst($event)">
-                        加入购物车
+                    <div class="cartcontrol-wrapper">
+                        <cartcontrol :food="food"></cartcontrol>
                     </div>
-                </transition>
+                    <transition name="fade">
+                        <div class="buy"  v-show="!food.count  || food.count === 0" @click.stop.prevent="addFirst($event)">
+                            加入购物车
+                        </div>
+                    </transition>
+                </div>
+
+                <split></split>
+
+                <div class="info" v-show="food.info">
+                    <h1 class="title">商品信息</h1>
+                    <p class="text">{{food.info}}</p>
+                </div>
+                
+                <split v-show="food.info"></split>
+
+                <div class="rating">
+                    <h1 class="title">商品评价</h1>
+
+                </div>
             </div>
         </div>
     </transition>
@@ -35,6 +49,7 @@
 import BScroll from 'better-scroll';
 import cartcontrol from '../cartcontrol/cartcontrol.vue';
 import Vue from 'vue';
+import split from '../split/split.vue';
 
 export default {
     props:{
@@ -71,7 +86,8 @@ export default {
         }
     },
     components:{
-        "cartcontrol":cartcontrol
+        "cartcontrol":cartcontrol,
+        "split":split
     }
 };
 </script>
@@ -117,6 +133,7 @@ export default {
         }
         .content{
             padding:18px;
+            position:relative;
             .title{
                 line-height:14px;
                 margin-bottom:8px;
@@ -151,35 +168,47 @@ export default {
                     color:rgb(147, 153, 159);
                 }
             }
-        }
-        .cartcontrol-wrapper{
-            position:absolute;
-            right:12px;
-            bottom: 12px;
-
-        }
-        .buy{
-            position:absolute;
-            right: 18px;
-            bottom:18px;
-            z-index:10;
-            height:24px;
-            line-height:24px;
-            padding:0 12px;
-            box-sizing:border-box;
-            font-size:10px;
-            border-radius: 12px;
-            color:#fff;
-            background:rgb(0,160,220);
-            opacity: 1;
-            &.fade-enter-active,&.fade-leave-active{
-                transition: all 1s;
+            .cartcontrol-wrapper{
+                position:absolute;
+                right:12px;
+                bottom: 12px;
             }
-            &.fade-enter,&.fade-leave-to{
-                opacity: 0;
+            .buy{
+                position:absolute;
+                right: 18px;
+                bottom:18px;
+                z-index:10;
+                height:24px;
+                line-height:24px;
+                padding:0 12px;
+                box-sizing:border-box;
+                font-size:10px;
+                border-radius: 12px;
+                color:#fff;
+                background:rgb(0,160,220);
+                opacity: 1;
+                &.fade-enter-active,&.fade-leave-active{
+                    transition: all 1s;
+                }
+                &.fade-enter,&.fade-leave-to{
+                    opacity: 0;
+                }   
             }
-
         }
-      
+        .info{
+            padding:18px;
+            .title{
+                line-height: 14px;
+                margin-bottom: 6px;
+                font-size: 14px;
+                color:rgb(7,17,27);
+            }
+            .text{
+                line-height: 24px;
+                padding:0 8px;
+                font-size: 12px;
+                color:rgb(77,85,93);
+            }
+        }
     }
 </style>
---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+const POSITIVE = 0;
+const NEGATIVE = 1;
+const ALL = 2;
+
+export default {
+    props:{
+        ratings:{ // 评价
+            type:Array,
+            default(){
+                return [];  // 数组和对象都要 default return
+            }
+        },
+        selectType:{ // 类型
+            type:Number,
+            default:ALL
+        },
+        onlyContent:{ // 读内容 不读内容
+            type:Boolean,
+            default:false
+        },
+        desc:{ // 描述
+            type:Object,
+            default(){
+                return {
+                    all: '全部',
+                    positive: '满意',
+                    negative: '不满意'
+                }
+            }
+        }
+    }
+}
+</script>
+<style lang="scss" scoped="" type="text/scss">
+
+</style>
